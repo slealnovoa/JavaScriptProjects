09d862fe8f051b543df827ddb96439471d1be08b
Santiago Leal <slealnov@my.centennialcollege.ca>
2024-05-12 15:15:36 -0400

    Add project files.

---
+++ BMRCalculator/wwwroot/js/site.js
@@ -0,0 +1,94 @@
+﻿function bmrMetric(age, weight, height, Gender, activity) {
+    event.preventDefault();
+    var age = parseInt(document.getElementById("age").value);
+    var weight = parseInt(document.getElementById("weight").value);
+    var height = parseInt(document.getElementById("height").value);
+    var Gender = document.getElementById("Gender").value;
+    var activity = document.getElementById("activity").value;
+    var bmr;
+
+    if (Gender === "Male") {
+        bmr = 66.5 + (13.76 * weight) + (5.003 * height) - (6.755 * age);
+    }
+    if (Gender === "Female") {
+        bmr = 655 + (9.563 * weight) + (1.850 * height) - (4.676 * age);
+    }
+    var caloriesPerDay;
+    switch (activity) {
+        case '1':
+            caloriesPerDay = bmr * 1.2;
+            break;
+
+        case '2':
+            caloriesPerDay = bmr * 1.375;
+            break;
+
+        case '3':
+            caloriesPerDay = bmr * 1.55;
+            break;
+
+        case '4':
+            caloriesPerDay = bmr * 1.725;
+            break;
+
+        case '5':
+            caloriesPerDay = bmr * 1.9;
+            break;
+    }
+    var bmrThreeDec = bmr.toFixed(3);
+    var caloriesPerDayThreeDec = caloriesPerDay.toFixed(3);
+
+    document.getElementById('resultBmr').innerHTML = bmrThreeDec;
+    document.getElementById('resultCal').innerHTML = caloriesPerDayThreeDec;
+}
+
+var submit = document.getElementById("submitMetric");
+submit.addEventListener("click", bmrMetric, false);
+
+function bmrImperial(heightImperial, ageImperial, activityImperial, GenderImperial, weightImperial) {
+    event.preventDefault();
+    var ageImperial = parseInt(document.getElementById("ageImperial").value);
+    var weightImperial = parseInt(document.getElementById("weightImperial").value);
+    var heightImperial = parseInt(document.getElementById("heightImperial").value);
+    var GenderImperial = document.getElementById("GenderImperial").value;
+    var activityImperial = document.getElementById("activityImperial").value;
+    var bmrImp;
+
+    if (GenderImperial === "Male") {
+        bmrImp = 66 + (6.2 * weightImperial) + (12.7 * heightImperial) - (6.76 * ageImperial);
+    }
+    if (GenderImperial === "Female") {
+        bmrImp = 655 + (4.35 * weightImperial) + (4.7 * heightImperial) - (4.7 * ageImperial)
+    }
+    var caloriesPerDayImp;
+    switch (activityImperial) {
+
+        case '1':
+            caloriesPerDayImp = bmrImp * 1.2;
+            break;
+
+        case '2':
+            caloriesPerDayImp = bmrImp * 1.375;
+            break;
+
+        case '3':
+            caloriesPerDayImp = bmrImp * 1.55;
+            break;
+
+        case '4':
+            caloriesPerDayImp = bmrImp * 1.725;
+            break;
+
+        case '5':
+            caloriesPerDayImp = bmrImp * 1.9;
+            break;
+    }
+    var bmrImp3Dec = bmrImp.toFixed(3)
+    var caloriesPerDayImp3Dec = caloriesPerDayImp.toFixed(3)
+
+    document.getElementById('resultBmr2').innerHTML = bmrImp3Dec;
+    document.getElementById('resultCal2').innerHTML = caloriesPerDayImp3Dec;
+}
+
+var submit = document.getElementById("submitImperial");
+submit.addEventListener("click", bmrImperial, false);
---
+++ InputValidation/wwwroot/js/site.js
@@ -0,0 +1,161 @@
+﻿var FName = document.getElementById("FName");
+var LName = document.getElementById("LName");
+var Address = document.getElementById("Address");
+var City = document.getElementById("City");
+var PCode = document.getElementById("PCode");
+var Province = document.getElementById("Province");
+var Age = document.getElementById("Age");
+
+var Password = document.getElementById("Password");
+var Confirm = document.getElementById("Confirm");
+
+var Email = document.getElementById("Email");
+
+var InputValues = document.querySelectorAll("input");
+var InputLabels = document.querySelectorAll("label");
+
+var errordiv = document.getElementById("error");
+var showAlert = true;
+
+var alertmessages = "";
+
+
+function showError(inputtxt, i) {
+    inputtxt.style.background = "rgb(255,233,233)";
+    inputtxt.setCustomValidity("All values are required");
+
+    if (showAlert) {
+        alertmessages += "All fields are required!!<br>"
+        showAlert = false;
+    }
+
+}
+
+function removeError(inputtxt) {
+    inputtxt.style.background = "";
+    inputtxt.setCustomValidity("");
+}
+
+function validate() {
+
+    if (document.contains(document.getElementsByClassName("alert")[0])) {
+        document.getElementsByClassName("alert")[0].remove();
+    }
+
+    if (document.contains(document.getElementsByClassName("success")[0])) {
+        document.getElementsByClassName("success")[0].remove();
+    }
+
+
+    showAlert = true;
+    alertmessages = "";
+
+    for (var i = 0; i < InputValues.length; i++) {
+        if (document.getElementById(InputValues[i].id).value.length == 0) {
+            showError(document.getElementById(InputValues[i].id), i);
+        } else {
+            removeError(document.getElementById(InputValues[i].id));
+        }
+    }
+
+    validateProstalCode()
+    validateProvince();
+    validateAge();
+
+    validateEmail();
+    validatePass();
+
+    matchPassword();
+
+    if (!alertmessages == "") {
+        var alertdiv = document.getElementById("alertdiv");
+
+        var alertmessage = document.createElement('div');
+        alertmessage.className = "alert";
+        alertmessage.innerHTML = alertmessages;
+
+        alertdiv.appendChild(alertmessage);
+
+    } else {
+        var alertdiv = document.getElementById("alertdiv");
+
+        var alertmessage = document.createElement('div');
+        alertmessage.className = "success";
+        alertmessage.innerHTML = "Thanks for registering with our website, your customer record was created successfully. ";
+
+        alertdiv.appendChild(alertmessage);
+    }
+}
+
+function validatePass() {
+    var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+    if (!re.test(Password.value) && Password.value != "") {
+        Password.style.background = "rgb(255,233,233)";
+        Confirm.style.background = "rgb(255,233,233)";
+        alertmessages += "Pasword must have at least 6 characters, contain at least one digit, one upper-case, one lower-case character, and special character<br>"
+    }
+}
+
+function matchPassword() {
+    if (Password.value != Confirm.value && Password.value != "" && Confirm.value != "") {
+        Confirm.style.background = "rgb(255,233,233)";
+        alertmessages += "Password and confirm password should be same<br>"
+    }
+}
+
+function validateEmail() {
+    var re = /\S+@\S+\.\S+/;
+    if (!re.test(Email.value) && Email.value != "") {
+        Email.style.background = "rgb(255,233,233)";
+        alertmessages += "Input email correctly (e.g. abc@xyz.com)<br>";
+    }
+}
+
+function validateAge() {
+    if (Age.value < 18 && Age.value != "") {
+        Age.style.background = "rgb(255,233,233)";
+        alertmessages += "Age must be atleast 18 years old<br>";
+    }
+}
+
+function validateProvince() {
+    const myStringArray = ["QC", "ON", "MN", "SK", "AB", "BC"];
+    if (!myStringArray.includes(Province.value) && Province.value != "") {
+        Province.style.background = "rgb(255,233,233)";
+        alertmessages += "The province " + Province.value + " is not allowed. Please enter: QC, ON, MN, SK, AB, or BC<br>";
+    }
+}
+
+function validateProstalCode() {
+    if (!PCode.value == 0) {
+        PCode.style.background = "rgb(255,233,233)";
+        postalCode = PCode.value.toString().trim();
+        var ca = new RegExp(/^[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ]( )?\d[ABCEGHJKLMNPRSTVWXYZ]\d$/i);
+
+        if (!ca.test(postalCode.toString())) {
+            alertmessages += "Enter postal code correctly<br>";
+        }
+    }
+}
+
+
+
+
+function createEventListeners() {
+
+    var submitbtn = document.getElementById("RegisterNow");
+
+    if (submitbtn.addEventListener) {
+        submitbtn.addEventListener("click", validate, false);
+    } else if (submitbtn.attachEvent) {
+        submitbtn.attachEvent("onclick", validate);
+    }
+}
+
+
+
+if (window.addEventListener) {
+    window.addEventListener("load", createEventListeners, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", createEventListeners);
+}
